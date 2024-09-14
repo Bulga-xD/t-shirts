a7ac1fb3340fab37d4c1841a0ec76afceab18d35
Chavdar <ch.tonchev94@gmail.com>
2024-09-14 23:58:49 +0300

    fix responsive

--- src/components/shared/users-reviews.tsx
+++ src/components/shared/users-reviews.tsx
@@ -4,14 +4,25 @@ import { ChevronLeft, ChevronRight } from "lucide-react";
 import { Card, CardContent, CardTitle } from "../ui/card";
 import Rating from "./product/rating";
 import { Button } from "../ui/button";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { UserReview as UserReviewType } from "@/types";
 
 const UserReviews = ({ reviews }: { reviews: UserReviewType[] }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [isMobile, setIsMobile] = useState(false);
 
-  const maxReviews = 3;
-  const cardWidth = 100 / maxReviews;
+  useEffect(() => {
+    updateIsMobile();
+    window.addEventListener("resize", updateIsMobile);
+    return () => window.removeEventListener("resize", updateIsMobile);
+  }, []);
+
+  const maxReviews = isMobile ? 1 : 3;
+  const cardWidth = isMobile ? 100 : 100 / maxReviews;
+
+  const updateIsMobile = () => {
+    setIsMobile(window.innerWidth < 768);
+  };
 
   const handleNext = () => {
     setCurrentIndex((prevIndex) =>
@@ -27,7 +38,7 @@ const UserReviews = ({ reviews }: { reviews: UserReviewType[] }) => {
 
   return (
     <section id="reviews" className="flex flex-col gap-4">
-      <div className="flex justify-between mb-10">
+      <div className="flex flex-col gap-4 lg:flex-row justify-between mb-10">
         <h2 className="text-3xl font-bold">
           Над 30,000+ Доволни Клиенти избрали VANDALL!
         </h2>
@@ -46,11 +57,18 @@ const UserReviews = ({ reviews }: { reviews: UserReviewType[] }) => {
             className="w-full py-4 px-1 grid grid-flow-col transition-transform duration-500 ease-in-out gap-4"
             style={{
               transform: `translateX(-${currentIndex * cardWidth}%)`,
-              gridAutoColumns: `calc(${cardWidth}% - 12px)`,
+              gridAutoColumns: isMobile
+                ? `calc(100% - 8px)`
+                : `calc(${cardWidth}% - 12px)`,
             }}
           >
             {reviews.map((review) => (
-              <div key={review.id} className="w-[calc(100% / 3)]">
+              <div
+                key={review.id}
+                className={`h-[220px] ${
+                  isMobile ? "w-full" : "w-[calc(100% / 3)]"
+                }`}
+              >
                 <UserReview
                   fullName={review.fullName!}
                   city={review.city || ""}
@@ -83,14 +101,16 @@ export const UserReview = ({
   text: string;
 }) => {
   return (
-    <Card className="p-4 hover:shadow-md transform transition-transform duration-300 hover:-translate-y-2 h-full">
+    <Card className="p-4 w-full overflow-hidden hover:shadow-md transform transition-transform duration-300 hover:-translate-y-2 h-full">
       <CardTitle className="text-base my-1">
         {fullName} - {city}
       </CardTitle>
       <Rating value={rating} />
       <h3 className="text-sm font-bold my-1">{title}</h3>
       <CardContent className="p-0">
-        <p className="text-sm text-muted-foreground">&rdquo;{text}&rdquo;</p>
+        <p className="text-sm text-muted-foreground line-clamp-5">
+          &rdquo;{text}&rdquo;
+        </p>
       </CardContent>
     </Card>
   );
